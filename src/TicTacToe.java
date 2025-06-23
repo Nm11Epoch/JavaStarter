f7beea78c6ed4d9fb6333d92486112df53724026
Nm11Epoch <nickmarti09@gmail.com>
2025-06-23 17:45:52 +0000

    Used Scanner to make basic shapes

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class TicTacToe{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("How llllong do you want the base of your triangle to be? ");
+        System.out.print("How long do you want the base of your triangle to be? ");
         int base = scan.nextInt();
         String plus = "";
         for (int x = 0; x < base; x ++) {
